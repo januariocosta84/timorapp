8ad39155eb929b11ad196e287d4e6ca5174cbc41
Januario da Costa <jnrdacosta@hotmail.com>
2024-10-09 17:15:21 +0900

    add count regitered users

--- parentregistrationApp/forms.py
+++ parentregistrationApp/forms.py
@@ -98,9 +98,9 @@ class ParentForm(forms.ModelForm):
         return user
 def send_otp(phone_number, otp_code):
     # Use environment variables for sensitive data
-    account_sid = os.getenv("TWILIO_ACCOUNT_SID", "")
-    auth_token = os.getenv("TWILIO_AUTH_TOKEN", "")
-    client = Client(account_sid, auth_token)
+    #account_sid = os.getenv("TWILIO_ACCOUNT_SID", "AC3704c56723579efe1364b541ae372c58")
+   #auth_token = os.getenv("TWILIO_AUTH_TOKEN", "9e9d3b380ca2489cfa37739fc496d954")
+    #client = Client(account_sid, auth_token)
 
     try:
         # Ensure the phone number has the correct format (with country code)
